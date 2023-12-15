316968436216e1f83259aee28fcb89741cd39b7f
Guilhermo Masid <guilhermomasid@gmail.com>
2023-12-15 13:19:02 -0300

    validate key lenght and if its alpha

--- 02-arrays/substitution.c
+++ 02-arrays/substitution.c
@@ -1,4 +1,15 @@
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
+
+bool is_string_alpha(char *string) {
+  for (int i = 0; i < strlen(string); i++) {
+    if (!isalpha(string[i])) return false;
+  }
+
+  return true;
+}
 
 int main(int argc, char **argv) {
   if (argc != 3) {
@@ -7,6 +18,14 @@ int main(int argc, char **argv) {
 
   char *key = argv[1];
   char *plaintext = argv[2];
+
+  if (strlen(key) != 26) {
+    return printf("Key must contain 26 characters.\n");
+  }
+
+  if (!is_string_alpha(key)) {
+    return printf("Key must only contain alphabetic characters.\n");
+  }
 
   printf("Key = %s | Text = %s\n", key, plaintext);
 }
--- 02-arrays/test.sh
+++ 02-arrays/test.sh
@@ -4,13 +4,13 @@ gcc substitution.c -o substitution
 # be in command line
 
 # Validate:
-# 1. Contains exactly 26 chars
-# 2. Contains only alphabetical chars
-# 3. Contains each letter exacly once
+# 1. Contains exactly 26 chars -> DONE
+# 2. Contains only alphabetical chars -> DONE
+# 3. Contains each letter exacly once ->
 
 
 ./substitution # "Usage: ./substitution <key> <plaintext>"
-./substitution YUIOLKP  # "Key must contain 26 characters."
+./substitution YUIOLKP "Hello!"  # "Key must contain 26 characters."
 ./substitution YTNSHKVEFXRBAUQZCLWDMIPGJ2 "Hello!" # "Key must only contain alphabetic characters."
 ./substitution YTNSHKVEYXRBAUQZCLWDMIPGJO "Hello!" # "Each char must appear just once"
 ./substitution YTNSHKVEFXRBAUQZCLWDMIPGJO "Hello!" # "ciphertext: Ehbbq!"
