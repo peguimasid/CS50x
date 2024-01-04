eb776dfbb936cd70be3be8b891c4590612d3200a
Guilhermo Masid <guilhermomasid@gmail.com>
2024-01-04 15:05:42 -0300

    improve code

--- 04-memory/02-image-filter/filter.c
+++ 04-memory/02-image-filter/filter.c
@@ -5,11 +5,10 @@
 #include "helpers.h"
 
 int main(int argc, char *argv[]) {
-  // Define allowable filters
   char *filters = "begr";
 
-  // Get filter flag and check validity
   char filter = getopt(argc, argv, filters);
+
   if (filter == '?') {
     printf("Invalid filter.\n");
     return 1;
