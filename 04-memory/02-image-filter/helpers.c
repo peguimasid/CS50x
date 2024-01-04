72d46b5361e12acec894387d7c21eb0042e12fa5
Guilhermo Masid <guilhermomasid@gmail.com>
2024-01-04 15:15:48 -0300

    grayscale working

--- 04-memory/02-image-filter/helpers.c
+++ 04-memory/02-image-filter/helpers.c
@@ -2,7 +2,19 @@
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width]) {
-  return;
+  for (int y = 0; y < height; y++) {
+    for (int x = 0; x < width; x++) {
+      BYTE red = image[y][x].rgbtRed;
+      BYTE green = image[y][x].rgbtGreen;
+      BYTE blue = image[y][x].rgbtBlue;
+
+      BYTE average = (red + green + blue) / 3;
+
+      image[y][x].rgbtRed = average;
+      image[y][x].rgbtGreen = average;
+      image[y][x].rgbtBlue = average;
+    }
+  }
 }
 
 // Reflect image horizontally
