1e90328e12b75b54e8c98d11bc744e63a55448ee
Guilhermo Masid <guilhermomasid@gmail.com>
2024-01-05 14:01:04 -0300

    edges working

--- 04-memory/02-image-filter/helpers.c
+++ 04-memory/02-image-filter/helpers.c
@@ -1,5 +1,6 @@
 #include "helpers.h"
 
+#include <math.h>
 #include <stdio.h>
 
 // Convert image to grayscale
@@ -73,7 +74,68 @@ void blur(int height, int width, RGBTRIPLE image[height][width]) {
   }
 }
 
+// color: 0 -> red | 1 -> green | 2 -> blue
+int getEdgeColor(int y, int x, int h, int w, RGBTRIPLE copy[h][w], int color) {
+  int gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+  int gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+
+  int gx_color = 0;
+  int gy_color = 0;
+
+  for (int i = -1; i <= 1; i++) {
+    for (int j = -1; j <= 1; j++) {
+      int rowIndex = y + i;
+      int colIndex = x + j;
+
+      if (rowIndex < 0 || rowIndex >= h || colIndex < 0 || colIndex >= w) {
+        gx_color += 0;
+        gy_color += 0;
+        continue;
+      }
+
+      RGBTRIPLE current_pixel = copy[rowIndex][colIndex];
+
+      if (color == 0) {
+        gx_color += gx[i + 1][j + 1] * current_pixel.rgbtRed;
+        gy_color += gy[i + 1][j + 1] * current_pixel.rgbtRed;
+      }
+
+      if (color == 1) {
+        gx_color += gx[i + 1][j + 1] * current_pixel.rgbtGreen;
+        gy_color += gy[i + 1][j + 1] * current_pixel.rgbtGreen;
+      }
+
+      if (color == 2) {
+        gx_color += gx[i + 1][j + 1] * current_pixel.rgbtBlue;
+        gy_color += gy[i + 1][j + 1] * current_pixel.rgbtBlue;
+      }
+    }
+  }
+
+  int result = sqrt((gx_color * gx_color) + (gy_color * gy_color));
+
+  if (result > 255) {
+    return 255;
+  }
+
+  return result;
+}
+
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width]) {
-  return;
+  RGBTRIPLE copy[height][width];
+
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      copy[i][j] = image[i][j];
+    }
+  }
+
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      image[i][j].rgbtRed = getEdgeColor(i, j, height, width, copy, 0);
+      image[i][j].rgbtGreen = getEdgeColor(i, j, height, width, copy, 1);
+      image[i][j].rgbtBlue = getEdgeColor(i, j, height, width, copy, 2);
+    }
+  }
 }
