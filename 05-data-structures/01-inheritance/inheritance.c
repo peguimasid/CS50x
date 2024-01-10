fae8b028b103c3ddb515080fb16c6061540ae940
Guilhermo Masid <guilhermomasid@gmail.com>
2024-01-10 12:57:58 -0300

    finish exercise

--- 05-data-structures/01-inheritance/inheritance.c
+++ 05-data-structures/01-inheritance/inheritance.c
@@ -35,7 +35,8 @@ int main(void) {
 
 // Create a new individual with `generations`
 person *create_family(int generations) {
-  // TODO: Allocate memory for new person
+  // ✅ TODO: Allocate memory for new person
+  person *new_person = malloc(sizeof(person));
 
   // If there are still generations left to create
   if (generations > 1) {
@@ -43,29 +44,41 @@ person *create_family(int generations) {
     person *parent0 = create_family(generations - 1);
     person *parent1 = create_family(generations - 1);
 
-    // TODO: Set parent pointers for current person
+    // ✅ TODO: Set parent pointers for current person
+    new_person->parents[0] = parent0;
+    new_person->parents[1] = parent1;
 
-    // TODO: Randomly assign current person's alleles based on the alleles of their parents
+    // ✅ TODO: Randomly assign current person's alleles based on the alleles of their parents
+    new_person->alleles[0] = parent0->alleles[rand() % 2];
+    new_person->alleles[1] = parent1->alleles[rand() % 2];
   }
 
   // If there are no generations left to create
   else {
-    // TODO: Set parent pointers to NULL
+    // ✅ TODO: Set parent pointers to NULL
+    new_person->parents[0] = NULL;
+    new_person->parents[1] = NULL;
 
-    // TODO: Randomly assign alleles
+    // ✅ TODO: Randomly assign alleles
+    new_person->alleles[0] = random_allele();
+    new_person->alleles[1] = random_allele();
   }
 
-  // TODO: Return newly created person
-  return NULL;
+  // ✅ TODO: Return newly created person
+  return new_person;
 }
 
 // Free `p` and all ancestors of `p`.
 void free_family(person *p) {
-  // TODO: Handle base case
+  // ✅ TODO: Handle base case
+  if (!p) return;
 
-  // TODO: Free parents recursively
+  // ✅ TODO: Free parents recursively
+  free_family(p->parents[0]);
+  free_family(p->parents[1]);
 
-  // TODO: Free child
+  // ✅ TODO: Free child
+  free(p);
 }
 
 // Print each family member and their alleles.
