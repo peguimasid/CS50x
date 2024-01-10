576d16ba70ff922b60198491c0b0ddeab20de804
Guilhermo Masid <guilhermomasid@gmail.com>
2024-01-10 16:59:02 -0300

    improving code

--- 05-data-structures/02-speller/speller.c
+++ 05-data-structures/02-speller/speller.c
@@ -11,24 +11,25 @@
 #undef calculate
 #undef getrusage
 
-// Default dictionary
 #define DICTIONARY "dictionaries/large"
 
-// Prototype
 double calculate(const struct rusage *b, const struct rusage *a);
 
 int main(int argc, char *argv[]) {
-  // Check for correct number of args
   if (argc != 2 && argc != 3) {
     printf("Usage: ./speller [DICTIONARY] text\n");
     return 1;
   }
 
   // Structures for timing data
-  struct rusage before, after;
+  struct rusage before;
+  struct rusage after;
 
   // Benchmarks
-  double time_load = 0.0, time_check = 0.0, time_size = 0.0, time_unload = 0.0;
+  double time_load = 0.0;
+  double time_check = 0.0;
+  double time_size = 0.0;
+  double time_unload = 0.0;
 
   // Determine dictionary to use
   char *dictionary = (argc == 3) ? argv[1] : DICTIONARY;
