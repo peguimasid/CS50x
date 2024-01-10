4cba9a4bc7e9b36e15991518731cac704970ee23
Guilhermo Masid <guilhermomasid@gmail.com>
2024-01-10 17:54:49 -0300

    implement functions, check case sensitive

--- 05-data-structures/02-speller/dictionary.c
+++ 05-data-structures/02-speller/dictionary.c
@@ -1,5 +1,3 @@
-// Implements a dictionary's functionality
-
 #include "dictionary.h"
 
 #include <ctype.h>
@@ -10,7 +8,6 @@
 
 typedef uint8_t BYTE;
 
-// Represents a node in a hash table
 typedef struct node {
   char word[LENGTH + 1];
   struct node *next;
@@ -18,24 +15,29 @@ typedef struct node {
 
 int num_words = 0;
 
-// TODO: Choose number of buckets in hash table
 const unsigned int N = 26;
 
-// Hash table
 node *table[N];
 
-// Returns true if word is in dictionary, else false
 bool check(const char *word) {
-  // TODO
+  unsigned int index = hash(word);
+
+  node *n = table[index];
+
+  while (n) {
+    if (strcmp(n->word, word) == 0) {
+      return true;
+    }
+    n = n->next;
+  }
+
   return false;
 }
 
-// Hashes word to a number
 unsigned int hash(const char *word) {
   return toupper(word[0]) - 'A';
 }
 
-// Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary) {
   FILE *source = fopen(dictionary, "r");
   BYTE buffer;
@@ -63,13 +65,19 @@ bool load(const char *dictionary) {
   return true;
 }
 
-// Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void) {
   return num_words;
 }
 
-// Unloads dictionary from memory, returning true if successful, else false
 bool unload(void) {
-  // TODO
-  return false;
+  for (int i = 0; i < N; i++) {
+    node *n = table[i];
+    while (n) {
+      node *tmp = n;
+      n = n->next;
+      free(tmp);
+    }
+  }
+
+  return true;
 }
