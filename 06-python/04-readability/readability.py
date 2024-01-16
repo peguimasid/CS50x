de601e9038d60064bf66c57c10854275cd4462eb
Guilhermo Masid <guilhermomasid@gmail.com>
2024-01-16 19:35:03 -0300

    count letters and words

---
+++ 06-python/04-readability/readability.py
@@ -0,0 +1,24 @@
+# (L of letters * 0.0588) - (S * 0.296) - 15.8
+# L is the average number of letters per 100 words in the text: that is, the number of letters divided by the number of words, all multiplied by 100.
+# S is the average number of sentences per 100 words in the text: that is, the number of sentences divided by the number of words, all multiplied by 100.
+# You can consider any sequence of characters that ends with a . or a ! or a ? to be a sentence.
+
+# Grade 1 - 16 - "Grade X"
+# Grade < 1 - "Before Grade 1"
+# Grade > 16 - "Grade 16+"
+
+
+def count_letters(text: str) -> int:
+    return len([c for c in text if c.isalpha()])
+
+
+def count_words(text: str) -> int:
+    return len(text.split(" "))
+
+
+def count_sentences(text: str) -> int:
+    print("TODO")
+
+
+print(count_letters("One fish. Two fish. Red fish. Blue fish."))
+print(count_words("One fish. Two fish. Red fish. Blue fish."))
