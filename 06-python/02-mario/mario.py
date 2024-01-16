a80f1d012e3dd27039152cc31504c4792876d8a2
Guilhermo Masid <guilhermomasid@gmail.com>
2024-01-16 17:30:37 -0300

    remove unnecessary variable

--- 06-python/02-mario/mario.py
+++ 06-python/02-mario/mario.py
@@ -1,10 +1,7 @@
 height = int(input("Height: "))
 
-start_spaces = height - 1
-
 for i in range(height):
-    print(" " * start_spaces, end="")
-    start_spaces -= 1
+    print(" " * (height - i - 1), end="")
     print("#" * (i + 1), end="")
     print("  ", end="")
     print("#" * (i + 1), end="")
