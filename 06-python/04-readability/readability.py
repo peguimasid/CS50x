ec6110b21b859192ded16748e71cfaf5ffe9d34c
Guilhermo Masid <guilhermomasid@gmail.com>
2024-01-16 19:53:53 -0300

    finish exercise - readability

--- 06-python/04-readability/readability.py
+++ 06-python/04-readability/readability.py
@@ -1,4 +1,4 @@
-# (L of letters * 0.0588) - (S * 0.296) - 15.8
+# (L * 0.0588) - (S * 0.296) - 15.8
 # L is the average number of letters per 100 words in the text: that is, the number of letters divided by the number of words, all multiplied by 100.
 # S is the average number of sentences per 100 words in the text: that is, the number of sentences divided by the number of words, all multiplied by 100.
 # You can consider any sequence of characters that ends with a . or a ! or a ? to be a sentence.
@@ -13,15 +13,27 @@ def count_letters(text: str) -> int:
 
 
 def count_words(text: str) -> int:
-    return len(text.split(" "))
+    return len([c for c in text if c.isspace()])
 
 
 def count_sentences(text: str) -> int:
     return len([c for c in text if (c == "." or c == "?" or c == "!")])
 
 
-letters = count_letters("One fish. Two fish. Red fish. Blue fish.")
-words = count_words("One fish. Two fish. Red fish. Blue fish.")
-sentences = count_sentences(
-    "One fish. Two fish. i want to take this! Red fish. Blue fish."
-)
+text = "Would you like them here or there? I would not like them here or there. I would not like them anywhere."
+
+letters = count_letters(text)
+words = count_words(text)
+sentences = count_sentences(text)
+
+L = letters / words * 100
+S = sentences / words * 100
+
+grade = (L * 0.0588) - (S * 0.296) - 15.8
+
+if grade < 1:
+    print("Before Grade 1")
+elif grade > 16:
+    print("Grade 16+")
+else:
+    print(f"Grade {round(grade)}")
